7df16d11390df8247618b1572048d1fc3e926072
yuanshenli <yuanshenlifml@gmail.com>
2019-07-26 14:52:48 -0700

    add file for params

--- HDNN/params.py
+++ HDNN/params.py
@@ -5,10 +5,10 @@ lr = 0.0001
 learning_rate_decay_steps = 1000
 learning_rate_decay_rate = 0.98
 
-batch_size = 32
+batch_size = 128
 input_size = 3
 seq_length = 10001
-cont_length = 10000
+cont_length = 100
 
 patience, num_trial = 0, 0
 max_patience, max_trial = 50, 50
